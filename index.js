5e9d4ddcceb5c04d6182a70fff46ebbe1e090532
nickbsche <nickboesche@googlemail.com>
2020-12-22 11:19:18 +0100

    Funktionalitäten alle gegeben

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-
-/*
-Dropzone.options.myAwesomeDropzone = {
-  autoProcessQueue: true,
-  maxFilesize: 10, //--> max. 10 MB pro Datei!
-  maxFiles: 1,
-  clickable: true,
-  acceptedFiles: 'image/*',
-  sending: function(file, xhr, formData) {
-         formData.append("nr", this.element.name);
-         
-  }
-      
-};*/
-
 // Neue Version
 Dropzone.options.myAwesomeDropzone = {
   autoProcessQueue: true,
@@ -34,33 +19,37 @@ Dropzone.options.myAwesomeDropzone = {
  };
 
 
-//Datumsmanipulation
-function newDate() {
+// Datumsmanipulation
+function myDate() {
   var x = document.getElementById("mydate").value;
   var newdate = x.slice(0, 4) + x.slice(5, 7) + x.slice(8, 10)
   var newdatecal = newdate - 20201200
 
-
   for (var i = 1; i <= newdatecal; i++) {
     document.querySelector(".circle-" + i.toString()).style.background = "#143d02"
+
   }
-  for (var i = newdatecal + 1; i > newdatecal && i < 25; i++) {
+  for (var i = newdatecal + 1;i < 25; i++) {
     document.querySelector(".circle-" + i.toString()).style.background = "#5a0101"
   }
-  for (var i = 1; i < newdatecal; i++) {
-    document.querySelectorAll(".open")[i].addEventListener("click", function() {
-      this.style.display = "none"
-      var choice = (this.className.slice(0, -5))
-      document.querySelector("." + choice + "-img").style.display = "block"
-    });
+  for (var i = 0; i < newdatecal; i++) {
+    document.querySelectorAll(".open")[i].removeAttribute("disabled")
   }
-  for (var i = newdatecal; i > newdatecal - 1 && i < 25; i++) {
-    document.querySelectorAll(".open")[i].addEventListener("click", function() {
-      this.style.display = "block"
-      var choice = (this.className.slice(0, -5))
-      document.querySelector("." + choice + "-img").style.display = "none"
-    });
+  for (var i = newdatecal; i < 24; i++) {
+    document.querySelectorAll(".open")[i].disabled = "true"
+    document.querySelectorAll(".open")[i].style.display = "block"
+    var choice = (document.querySelectorAll(".open")[i].className.slice(0, -5))
+    console.log("." + choice + "-img")
+    document.querySelector("." + choice + "-img").style.display = "none"
   }
+}
+
+for (var i = 0; i<24; i++) {
+  document.querySelectorAll(".open")[i].addEventListener("click", function() {
+    this.style.display = "none"
+    var choice = (this.className.slice(0, -5))
+    document.querySelector("." + choice + "-img").style.display = "block"
+  });
 }
 
 
@@ -79,18 +68,14 @@ document.getElementById("mydate").value = today;
 
 
 // Datum manipulieren
-function newDate() {
-  var x = document.getElementById("mydate").value;
-  alert(x);
-}
-
+// Check which open
 var date = checkDoor - 20201200;
-for (var i = 0; i<date; i++) {
-  document.querySelectorAll(".open")[i].addEventListener("click", function() {
-    this.style.display = "none"
-    var choice = (this.className.slice(0, -5))
-    document.querySelector("." + choice + "-img").style.display = "block"
-  });
+for (var i = date; i < 24; i++) {
+  document.querySelectorAll(".open")[i].disabled = "true"
+  document.querySelectorAll(".open")[i].style.display = "block"
+  var choice = (document.querySelectorAll(".open")[i].className.slice(0, -5))
+  console.log("." + choice + "-img")
+  document.querySelector("." + choice + "-img").style.display = "none"
 }
 
 for (var i = 1; i <= date; i++) {
@@ -101,6 +86,7 @@ for (var i = 1; i <= date; i++) {
 
 // 2 Buttons für Admin Mode an und aus
 function activateAdmin() {    //Admin-Mode an
+/*
 var openButtons = document.querySelectorAll(".open");
 i = 0;
 l = openButtons.length;
@@ -108,7 +94,7 @@ l = openButtons.length;
 for (i; i < l; i++) {
   openButtons[i].style.display = "none";
 }
-
+*/
 var uploadButtons = document.querySelectorAll(".dropfield");
 i = 0;
 l = uploadButtons.length;
@@ -116,13 +102,11 @@ l = uploadButtons.length;
 for (i; i < l; i++) {
   uploadButtons[i].style.display = "block";
 }
-
-document.getElementById("mydate").disabled = false;
-document.getElementById("submitDate").disabled = false;
 } 
 
 
 function deactivateAdmin() {    //Admin-Mode aus
+/*
 var openButtons = document.querySelectorAll(".open");
 i = 0;
 l = openButtons.length;
@@ -130,7 +114,7 @@ l = openButtons.length;
 for (i; i < l; i++) {
   openButtons[i].style.display = "block";
 }
-
+*/
 var uploadButtons = document.querySelectorAll(".dropfield");
 i = 0;
 l = uploadButtons.length;
@@ -138,7 +122,4 @@ l = uploadButtons.length;
 for (i; i < l; i++) {
   uploadButtons[i].style.display = "none";
 }
-
-document.getElementById("mydate").disabled = true;
-document.getElementById("submitDate").disabled = true;
 }
